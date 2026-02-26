74453400165ccd64c8c37519bfa49f688f796f23
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-02-26 19:38:07 +0100

    Implement DAC initialization function
    Initialize the DAC by configuring the necessary clock and power settings. 
    & LIST OF REF TO UM for later work.

--- DAC/dac.c
+++ DAC/dac.c
@@ -1,1 +1,20 @@
-AD
+#include "LPC845.h"
+
+
+void DAC_Init(void){
+    SYSCON->SYSAHBCLKCTRL0 |= (1 << 28);
+    IOCON->PIO[17] &= ~(1 << 17);   
+    SYSCON->PDRUNCFG &= ~(1 << 13);
+}
+
+// System clock control
+// SYSAHBCLKCTRL0 (ADD 0x4004 8080) (UM_CH_8_Table_147)
+// SYSAHBCLKCTRL1 (ADD 0x4004 8084) (UM_CH_8_Table_148)
+// PERIPHERAL CLOCK SOURCE (UM_CH_8_Table_151)
+
+// Power configuration
+// PDRUNCFG (ADD 0x4004 8238) (UM_CH_8_Table_173)
+
+// Preset control
+// PRESETCTRL0 (ADD 0x4004 8088) (UM_CH_8_Table_149)
+// PRESETCTRL1 (ADD 0x4004 808C) (UM_CH_8_Table_150)
