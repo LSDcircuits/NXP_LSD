54dedb179761e2425156aadc8a395de9e6b1c76f
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-02-27 16:48:46 +0100

    Create i2c.c
    mainly to have he regs present already.

---
+++ I2C/i2c.c
@@ -0,0 +1,20 @@
+#include "LPC845.h"
+
+void I2C_Init(void)
+{
+    SYSCON->SYSAHBCLKCTRL0 |= (1 << 5);  // I2C0 clock
+
+    // Assign pins via SWM
+    SWM->PINASSIGN0 = (10 << 0) | (11 << 8);
+
+    // Enable open drain
+    IOCON->PIO0_10 |= (1 << 10);
+    IOCON->PIO0_11 |= (1 << 10);
+
+    // Clock config
+    SYSCON->I2C0CLKSEL = 0;
+    I2C0->CLKDIV = 30;
+
+    // Enable master
+    I2C0->CFG |= 1;
+}
