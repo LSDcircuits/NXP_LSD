3f2407bf866bd0e953963a006f9438eb10aacf80
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-02-27 17:14:24 +0100

    Implement DAC_Write function and clean up DAC_Init
    Added DAC_Write function to write values to DAC0.
    Initial

--- DAC/dac.c
+++ DAC/dac.c
@@ -1,20 +1,15 @@
 #include "LPC845.h"
 
-
 void DAC_Init(void){
     SYSCON->SYSAHBCLKCTRL0 |= (1 << 28);
-    IOCON->PIO[17] &= ~(1 << 17);   
+    IOCON->PIO[17] |= (1 << 16);   // disable digital mode for analog
     SYSCON->PDRUNCFG &= ~(1 << 13);
+    SWM0->PINENABLE0 &= ~(1 << 26);
+    DAC0->CTRL = 0;
 }
 
-// System clock control
-// SYSAHBCLKCTRL0 (ADD 0x4004 8080) (UM_CH_8_Table_147)
-// SYSAHBCLKCTRL1 (ADD 0x4004 8084) (UM_CH_8_Table_148)
-// PERIPHERAL CLOCK SOURCE (UM_CH_8_Table_151)
-
-// Power configuration
-// PDRUNCFG (ADD 0x4004 8238) (UM_CH_8_Table_173)
-
-// Preset control
-// PRESETCTRL0 (ADD 0x4004 8088) (UM_CH_8_Table_149)
-// PRESETCTRL1 (ADD 0x4004 808C) (UM_CH_8_Table_150)
+void DAC_Write(uint16_t value)
+{
+    value &= 0x3FF;
+    DAC0->CR = (value << 6);
+}
