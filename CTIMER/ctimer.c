f076919e33d0380850973822e14979e96d1eca4b
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-03-01 19:57:51 +0100

    Update CTIMER references to CTIMER0

--- CTIMER/ctimer.c
+++ CTIMER/ctimer.c
@@ -2,26 +2,26 @@
 void timer_init(void)
 {
     // 1. Enable clock to CTIMER
-    SYSCON->SYSAHBCLKCTRL0 |= (1 << 24); 
+    SYSCON->SYSAHBCLKCTRL0 |= (1 << 24);
     // 2. Reset CTIMER
     SYSCON->PRESETCTRL0 &= ~(1 << 24);
     SYSCON->PRESETCTRL0 |=  (1 << 24);
 
     // 3. Put timer in timer mode 
-    CTIMER->CTCR = 0x0;
+    CTIMER0->CTCR = 0x0;
     // 4. Prescaler 
-    CTIMER->PR = 0;
+    CTIMER0->PR = 0;
     // 5. Reset counter
-    CTIMER->TCR = (1 << 1); // Reset
-    CTIMER->TCR = 0;        // Release reset, keep disabled
+    CTIMER0->TCR = (1 << 1); // Reset
+    CTIMER0->TCR = 0;        // Release reset, keep disabled
 }
 
 void timer_start(void){
     // 6. Start timer
-    CTIMER->TCR |= (1 << 0);
+    CTIMER0->TCR |= (1 << 0);
 }
 
 void timer_stop(void){
     // 6. Start timer
-    CTIMER->TCR &= ~(1 << 0);
+    CTIMER0->TCR &= ~(1 << 0);
 }
