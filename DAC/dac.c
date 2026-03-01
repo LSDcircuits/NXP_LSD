8bf208ea804ccab5a375992879a18e4f4e5d248d
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-03-01 06:43:12 +0100

    Refactor DAC initialization and writing functions

--- DAC/dac.c
+++ DAC/dac.c
@@ -1,15 +1,13 @@
 #include "LPC845.h"
 
 void DAC_Init(void){
-    SYSCON->SYSAHBCLKCTRL0 |= (1 << 28);
-    IOCON->PIO[17] |= (1 << 16);   // disable digital mode for analog
-    SYSCON->PDRUNCFG &= ~(1 << 13);
-    SWM0->PINENABLE0 &= ~(1 << 26);
-    DAC0->CTRL = 0;
-}
-
-void DAC_Write(uint16_t value)
-{
+    SYSCON->SYSAHBCLKCTRL0 |= (1 << 28); // enable clock for DAC block (UM_CH_8_Table_147)
+    IOCON->PIO[17] |= (1 << 16);        // disable digital on Pin17  
+    SYSCON->PDRUNCFG &= ~(1 << 13);    // Power the Anlog device (UM_CH_8_Table_173)
+    SWM0->PINENABLE0 &= ~(1 << 26);   // Set Switch matrix for DAC (UM_CH_7_Table_196)
+    DAC0->CTRL = 0;                  // Set mode (UM_CH_28_Table_471)
+}  
+void DAC_Write(uint16_t value){
     value &= 0x3FF;
-    DAC0->CR = (value << 6);
-}
+    DAC0->CR = (value << 6); //(UM_CH_28_Table_470)
+} 
