5a46247237ce9e6a7778db2116fab4b2d4a62f43
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-03-02 18:39:23 +0100

    Fix DAC initialization to disable digital on Pin17

--- DAC/dac.c
+++ DAC/dac.c
@@ -2,7 +2,7 @@
 
 void DAC_Init(void){
     SYSCON->SYSAHBCLKCTRL0 |= (1 << 28); // enable clock for DAC block (UM_CH_8_Table_147)
-    IOCON->PIO[17] |= (1 << 16);        // disable digital on Pin17  
+    IOCON->PIO[17] |= (1 << 17);        // disable digital on Pin17  
     SYSCON->PDRUNCFG &= ~(1 << 13);    // Power the Anlog device (UM_CH_8_Table_173)
     SWM0->PINENABLE0 &= ~(1 << 26);   // Set Switch matrix for DAC (UM_CH_7_Table_196)
     DAC0->CTRL = 0;                  // Set mode (UM_CH_28_Table_471)
