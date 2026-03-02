5441506fe877ab667670efb7d2ec3728345e62e9
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-03-02 18:25:44 +0100

    Refactor timer functions to Ctimer prefix
    corrected some bits and added function to output the reg value for Ctimer TC.
    This will be used for simple timer for timing in between software.

--- CTIMER/ctimer.c
+++ CTIMER/ctimer.c
@@ -1,24 +1,26 @@
-void timer_init(void) {
-    // Enable clock to CTIMER
-    SYSCON->SYSAHBCLKCTRL0 |= (1 << 24);
-    // Reset CTIMER
-    SYSCON->PRESETCTRL0 &= ~(1 << 24);
-    SYSCON->PRESETCTRL0 |=  (1 << 24);
-    // Put timer in timer mode 
+void Ctimer_init(void) {
+    SYSCON->SYSAHBCLKCTRL0 |= (1 << 25);
+    SYSCON->PRESETCTRL0 &= ~(1 << 25);
+    SYSCON->PRESETCTRL0 |=  (1 << 25);
     CTIMER0->CTCR = 0x0;
-    // Prescaler 
     CTIMER0->PR = 0;
-    // Reset counter
-    CTIMER0->TCR = (1 << 1); // Reset
-    CTIMER0->TCR = 0;        // Release reset, keep disabled
+    CTIMER0->TCR = (1 << 1); 
+    CTIMER0->TCR = 0;        
 }
 
-void timer_start(void){
-    // Start timer
+void Ctimer_start(void){
     CTIMER0->TCR |= (1 << 0);
 }
 
-void timer_stop(void){
-    // Stop timer
+void Ctimer_stop(void){
     CTIMER0->TCR &= ~(1 << 0);
+}
+
+uint32_t read_timer(){
+    return CTIMER0->TC;
+}
+
+void clear_timer(){
+    CTIMER0->TCR &= ~(1 << 0);
+    CTIMER0->TCR |= (1 << 1); 
 }
