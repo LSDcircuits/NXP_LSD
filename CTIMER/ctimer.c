bcb4d17b0c611e0380c71c522240583abc121430
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-03-02 17:35:53 +0100

    Refactor timer_init function comments for clarity

--- CTIMER/ctimer.c
+++ CTIMER/ctimer.c
@@ -1,27 +1,24 @@
-
-void timer_init(void)
-{
-    // 1. Enable clock to CTIMER
+void timer_init(void) {
+    // Enable clock to CTIMER
     SYSCON->SYSAHBCLKCTRL0 |= (1 << 24);
-    // 2. Reset CTIMER
+    // Reset CTIMER
     SYSCON->PRESETCTRL0 &= ~(1 << 24);
     SYSCON->PRESETCTRL0 |=  (1 << 24);
-
-    // 3. Put timer in timer mode 
+    // Put timer in timer mode 
     CTIMER0->CTCR = 0x0;
-    // 4. Prescaler 
+    // Prescaler 
     CTIMER0->PR = 0;
-    // 5. Reset counter
+    // Reset counter
     CTIMER0->TCR = (1 << 1); // Reset
     CTIMER0->TCR = 0;        // Release reset, keep disabled
 }
 
 void timer_start(void){
-    // 6. Start timer
+    // Start timer
     CTIMER0->TCR |= (1 << 0);
 }
 
 void timer_stop(void){
-    // 6. Start timer
+    // Stop timer
     CTIMER0->TCR &= ~(1 << 0);
 }
