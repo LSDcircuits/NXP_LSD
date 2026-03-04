15d0501c910dd5a5063c034a77b12ee49313228d
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-03-04 18:16:36 +0100

    Update PWM.c

--- PWM/PWM.c
+++ PWM/PWM.c
@@ -1,5 +1,41 @@
 #include "LPC845.h"
 
+// counters:
+// 16 bit or 32 bit
+
+// Match registers:
+// Match conditions:
+
+// Counter:
+// counter value can be used in conjection with match registers
+// to create time proportioned PWM signal.
+
+// For output:
+// SCT_OUT0_O () -> value is the Pin assigned table 188
+
+// SCTIMER0->CONFIG &= ~(1 << 0) | UNIFY = 0 | TWO 16 bit timers
+// SCTIMER0->CONFIG |=  (1 << 0) | UNIFY = 1 | SINGLE 32 bit timer
+
+
+
+// 1. Enable    | SYSAHBCLKCTRL	    | bit 8
+// 2. Clear     | PRESETCTRL	    | bit 8 
+// 3. Interrupt | NVIC		        | 
+// 4. input     | INPUT MUX	        | 
+// 5. output    | SWIM   		    |
+// 6. Request   | DMA_ITRIG_PINMUX  |
+
+
+// SCTIMER CONFIG:
+
+// SCTIMER0->CONFIG &= ~(1 << 0) | UNIFY = 0 | TWO 16 bit timers  
+// SCTIMER0->CONFIG |=  (1 << 0) | UNIFY = 1 | SINGLE 32 bit timer
+
+
+
+SCTIMER CTRL 
+
+SCTIMER0->CTRL |= (1 << 4);
 void pwm_init(void){
     SYSCON->SYSAHBCLKCRTL0 |= (1 << 8); // clock enable SCT
     SYSCON->PRESETCTRL0 &= ~(1 << 8); // Reset values 
