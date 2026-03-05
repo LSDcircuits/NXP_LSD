65df5d55cab00e50f109c08d6ad067de176ebc72
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-03-05 07:19:46 +0100

    Update PWM.c

--- PWM/PWM.c
+++ PWM/PWM.c
@@ -34,10 +34,22 @@
 // If UNIFY = 1 in the CONFIG register, only the _L bits of this register are used. In this case,
 // REGMODE_H is not used.
 // find match registers
+
+| Field        | Purpose                                        |
+| ------------ | ---------------------------------------------- |
+| **MATCHSEL** | Select which MATCH register triggers the event |
+| **COMBMODE** | How match/input conditions combine             |
+| **STATELD**  | Load a new state (not needed for simple PWM)   |
+| **STATEV**   | State value to load                            |
+| **IOSEL**    | Input pin selection                            |
+| **IOCOND**   | Input condition                                |
+| **OUTSEL**   | Output selection                               |
+| **IOEN**     | Enable input trigger                           |
+| **MATCHMEM** | Enable match trigger                           |
+
 SCTIMER CTRL 
 
 SCTIMER0->CTRL |= (1 << 4);
-
 
 void pwm_init(void){
     SYSCON->SYSAHBCLKCRTL0 |= (1 << 8); // clock enable SCT
