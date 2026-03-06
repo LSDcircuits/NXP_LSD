230a58c17c397d4ab08371cd843a31ec13c8acfb
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-03-06 06:12:24 +0100

    Update TEMP_PWM.c

--- PWM/TEMP_PWM.c
+++ PWM/TEMP_PWM.c
@@ -5,37 +5,37 @@
 
 void pwm_init(void)
 {
-    /* Enable clock to SCT */
+    // enable
     SYSCON->SYSAHBCLKCTRL0 |= (1 << 8);
 
-    /* Reset SCT */
+    // reset
     SYSCON->PRESETCTRL0 &= ~(1 << 8);
     SYSCON->PRESETCTRL0 |=  (1 << 8);
 
-    /* Unified counter mode */
+    // countr 32 bit
     SCT0->CONFIG = (1 << 0);
 
-    /* Use system clock */
+    // use system clcok 
     SCT0->CTRL &= ~(1 << 2);
 
-    /* MATCH registers */
+    //match regs
     SCT0->MATCHREL[0].U = PWM_PERIOD;   // Period      | MATCHREL0 TABLE409
     SCT0->MATCHREL[1].U = PWM_DUTY;     // Duty cycle  |
 
-    /* Event 0 -> period reset */
+    // EVENT 0
     SCT0->EV[0].STATE = 0xFFFFFFFF;
     SCT0->EV[0].CTRL  = (0 << 0) | (1 << 12);  
 
-    /* Event 1 -> duty event */
+    // EVENY 1 
     SCT0->EV[1].STATE = 0xFFFFFFFF;
     SCT0->EV[1].CTRL  = (1 << 0);
 
-    /* Output 0 SET on event0 */
+    // Output 0 SET on event
     SCT0->OUT[0].SET = (1 << 0);
 
-    /* Output 0 CLEAR on event1 */
+    // Output 0 CLEAR on event1 
     SCT0->OUT[0].CLR = (1 << 1);
 
-    /* Start counter */
+    // Start counter
     SCT0->CTRL &= ~(1 << 2);
 }
