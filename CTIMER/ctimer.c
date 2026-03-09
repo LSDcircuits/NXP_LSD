871fa0de850aebb4240cfd66ba8213ecdc894460
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-03-09 06:56:27 +0100

    Update ctimer.c

--- CTIMER/ctimer.c
+++ CTIMER/ctimer.c
@@ -1,23 +1,23 @@
 void Ctimer_init(void) {
-    SYSCON->SYSAHBCLKCTRL0 |= (1 << 25);
-    SYSCON->PRESETCTRL0 &= ~(1 << 25);
-    SYSCON->PRESETCTRL0 |=  (1 << 25);
-    CTIMER0->CTCR = 0x0;
-    CTIMER0->PR = 0;
-    CTIMER0->TCR = (1 << 1); 
+    SYSCON->SYSAHBCLKCTRL0 |= (1 << 25); // (Table 95 – SYSAHBCLKCTRL0 register)
+    SYSCON->PRESETCTRL0 &= ~(1 << 25);   // (Table 89 – PRESETCTRL0 register)
+    SYSCON->PRESETCTRL0 |=  (1 << 25);   
+    CTIMER0->CTCR = 0x0;                 // (Table 701 – CTCR register)
+    CTIMER0->PR = 0;                     // (Table 699 – PR register)
+    CTIMER0->TCR = (1 << 1);             // Table 697 – TCR register
     CTIMER0->TCR = 0;        
 }
 
 void Ctimer_start(void){
-    CTIMER0->TCR |= (1 << 0);
+    CTIMER0->TCR |= (1 << 0);           // (Table 697 – TCR register)
 }
 
 void Ctimer_stop(void){
-    CTIMER0->TCR &= ~(1 << 0);
+    CTIMER0->TCR &= ~(1 << 0);        
 }
 
 uint32_t read_timer(){
-    return CTIMER0->TC;
+    return CTIMER0->TC;                // (Table 698 – TC register)
 }
 
 void clear_timer(){
