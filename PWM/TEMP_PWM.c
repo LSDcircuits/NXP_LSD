876907035af7042fd930a287066cd9db809c1436
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-03-18 10:05:03 +0100

    Update TEMP_PWM.c

--- PWM/TEMP_PWM.c
+++ PWM/TEMP_PWM.c
@@ -27,16 +27,15 @@ void pwm_init(void) {
     
     // Event 0: Match 0 → SET output (start of period)
     SCT0->EV[0].STATE = 0xFFFFFFFF;
-    SCT0->EV[0].CTRL  = (0 << 0)   // Match 0
-                      | (1 << 12); // Match condition only
+    SCT0->EV[0].CTRL  = (0 << 0) | (1 << 12); // Match 0 with timer condition only
     
     // Event 1: Match 1 → CLEAR output (end of duty)
     SCT0->EV[1].STATE = 0xFFFFFFFF;
-    SCT0->EV[1].CTRL  = (1 << 0);  // Match 1
+    SCT0->EV[1].CTRL  = (1 << 0) | ( 1 << 12;  // Match 1 with timer condition only
     
     // Output 0: SET on EV0, CLEAR on EV1
-    SCT0->OUT[0].SET = (1 << 0);
-    SCT0->OUT[0].CLR = (1 << 1);
+    SCT0->OUT[0].SET = (1 << 0); // when event 0 happens set  
+    SCT0->OUT[0].CLR = (1 << 1); // when event 1 happens clear
     
     // Route SCT_OUT0 to pin PIO0_10
     // PINASSIGN7: SCT_OUT0_O is bits [31:24] (not [23:16]!)
