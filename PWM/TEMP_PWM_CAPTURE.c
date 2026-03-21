59e19b002384c748d5ce266bee88acaaf2553037
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-03-21 07:04:41 +0100

    Create TEMP_PWM_CAPTURE.c
    config start made by KIMIK2, to be checked.

---
+++ PWM/TEMP_PWM_CAPTURE.c
@@ -0,0 +1,19 @@
+// Event 0: Capture on rising edge, then toggle between states
+SCT0->EV[0].CTRL = (0 << 0)       // CAP0
+                 | (1 << 4)       // SCT_PIN0
+                 | (1 << 5)       // RISING EDGE
+                 | (1 << 6);      // COMBINE
+
+// State machine: alternate between capturing and waiting
+SCT0->EV[0].STATE = (1 << 0);     // Enabled in state 0
+SCT0->EV[0].STATE |= (1 << 12);   // Set state 1 after event (next capture waits)
+
+SCT0->EV[1].CTRL = (0 << 0)       // Also CAP0 (or CAP1)
+                 | (1 << 4)       
+                 | (1 << 5)       // RISING EDGE
+                 | (1 << 6);
+SCT0->EV[1].STATE = (1 << 1);     // Enabled in state 1
+SCT0->EV[1].STATE |= (2 << 12);   // Return to state 0
+
+// This ping-pongs between states, capturing consecutive rising edges
+// in CAP0 and CAP1 (or overwriting and letting software track)
