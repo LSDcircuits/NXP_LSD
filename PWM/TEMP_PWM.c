4e70755756559ef7fa2d1a1e49d5a3021c93c0da
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-04-16 10:26:43 +0200

    Update TEMP_PWM.c
    Updated struct, eliminated un needed memory used to set duty. since its stored in the MATCHREL accessing directly is more efficient.

--- PWM/TEMP_PWM.c
+++ PWM/TEMP_PWM.c
@@ -19,12 +19,6 @@ typedef struct {
     uint32_t pin4;
     uint32_t pin5;
     uint32_t pin6;
-    uint32_t duty1;
-    uint32_t duty2;
-    uint32_t duty3;
-    uint32_t duty4;
-    uint32_t duty5;
-    uint32_t duty6;
     uint32_t period;
 } pwm_var;
 
@@ -36,27 +30,18 @@ void pwm_init(pwm_var *pwm_var) {
     // Reset SCT
     SYSCON->PRESETCTRL0 &= ~(1 << 8);
     SYSCON->PRESETCTRL0 |=  (1 << 8);
-    
-    // Configure pin as OUTPUT before routing
-    // GPIO->DIR[0] |= (1 << pwm_var->pin1);  //   
-    // GPIO->DIR[0] |= (1 << pwm_var->pin2);  // 
-    // GPIO->DIR[0] |= (1 << pwm_var->pin3);  // 
-    // GPIO->DIR[0] |= (1 << pwm_var->pin4);  // 
-    // GPIO->DIR[0] |= (1 << pwm_var->pin5);  // 
-    // GPIO->DIR[0] |= (1 << pwm_var->pin6);  // 
- 
     // Configure SCT: unified 32-bit counter, auto-limit
     SCT0->CONFIG = (1 << 0)      // UNIFY = 1 (32-bit counter)
                  | (1 << 17);    // AUTOLIMIT_L (reset at MATCH0)
 
     SCT0->MATCHREL[0] = pwm_var->period - 1;  // Period
 
-    SCT0->MATCHREL[1] = pwm_var->duty1;  // Duty cycle
-    SCT0->MATCHREL[2] = pwm_var->duty2;
-    SCT0->MATCHREL[3] = pwm_var->duty3;
-    SCT0->MATCHREL[4] = pwm_var->duty4;
-    SCT0->MATCHREL[5] = pwm_var->duty5;
-    SCT0->MATCHREL[6] = pwm_var->duty6;
+    SCT0->MATCHREL[1] = 0;  // Duty set to 0 for startup
+    SCT0->MATCHREL[2] = 0;
+    SCT0->MATCHREL[3] = 0;
+    SCT0->MATCHREL[4] = 0;
+    SCT0->MATCHREL[5] = 0;
+    SCT0->MATCHREL[6] = 0;
 
     // Stop and clar counter
     SCT0->CTRL = (1 << 2) | (1 << 3);  // HALT | CLRCTR (write both at once)
@@ -90,8 +75,8 @@ void pwm_init(pwm_var *pwm_var) {
     // Output 1: SET on EV0, CLEAR on EV1, to be done to the other 6 
 
     //CH0
-    SCT0->OUT[0].SET = (1 << 0); 
-    SCT0->OUT[0].CLR = (1 << 1); 
+    SCT0->OUT[0].SET = (1 << 0); // OUT[n] (set OUT high, (1 << EV[n]))
+    SCT0->OUT[0].CLR = (1 << 1); // OUT[n] (set OUT LOW, (1 << EN[n]))
     //CH1
     SCT0->OUT[1].SET = (1 << 0); 
     SCT0->OUT[1].CLR = (1 << 2); 
@@ -107,7 +92,9 @@ void pwm_init(pwm_var *pwm_var) {
 
     SCT0->OUT[5].SET = (1 << 0); 
     SCT0->OUT[5].CLR = (1 << 6); 
-    
+
+
+    // to be configured for input and output. 
     // Route SCT_OUT0 to pin
     SWM0->PINASSIGN.PINASSIGN7 = (SWM0->PINASSIGN.PINASSIGN7 & 0x00FFFFFF) 
                                 | (pwm_var->pin1 << 24); // SCT0
@@ -131,19 +118,12 @@ void set_pwm_freq(pwm_var *pwm_var, uint32_t freq) {
 }
 
 void set_pwm_duty(pwm_var *pwm_var, uint8_t pwm_duty[6]){
-    pwm_var->duty1 = (pwm_var->period * pwm_duty[0]/100);
-    pwm_var->duty2 = (pwm_var->period * pwm_duty[1]/100);
-    pwm_var->duty3 = (pwm_var->period * pwm_duty[2]/100);
-    pwm_var->duty4 = (pwm_var->period * pwm_duty[3]/100);
-    pwm_var->duty5 = (pwm_var->period * pwm_duty[4]/100);
-    pwm_var->duty6 = (pwm_var->period * pwm_duty[5]/100);
-
-    SCT0->MATCHREL[1] = pwm_var->duty1;  // Duty cycle
-    SCT0->MATCHREL[2] = pwm_var->duty2;
-    SCT0->MATCHREL[3] = pwm_var->duty3;
-    SCT0->MATCHREL[4] = pwm_var->duty4;
-    SCT0->MATCHREL[5] = pwm_var->duty5;
-    SCT0->MATCHREL[6] = pwm_var->duty6;
+    SCT0->MATCHREL[1] = (pwm_var->period * pwm_duty[0]/100);
+    SCT0->MATCHREL[2] = (pwm_var->period * pwm_duty[0]/100);
+    SCT0->MATCHREL[3] = (pwm_var->period * pwm_duty[0]/100);
+    SCT0->MATCHREL[4] = (pwm_var->period * pwm_duty[0]/100);
+    SCT0->MATCHREL[5] = (pwm_var->period * pwm_duty[0]/100);
+    SCT0->MATCHREL[6] = (pwm_var->period * pwm_duty[0]/100);
 }
 
 void disable_pwm(void) { 
